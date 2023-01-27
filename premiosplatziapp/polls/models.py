7b6e4891afaaf98e685944beb023da991edeab7f
Santiago Arias <santi99oca@gmail.com>
2023-01-27 00:19:20 -0700

    Question and Choice models added

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+# Model for questions table in the db
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+# Model for choices table in the db with Foreign key a question
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
