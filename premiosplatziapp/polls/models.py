85e3a5dc7176af1be2007bff0bb00ad8f47a4a62
Santiago Arias <santi99oca@gmail.com>
2023-01-27 01:01:02 -0700

    time-auto added in Question model

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Model for questions table in the db
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
 
     def __str__(self):
         return self.question_text
