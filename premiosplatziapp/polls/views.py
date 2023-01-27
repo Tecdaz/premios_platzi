72f485b6c17e12cb3a4f65bcadacf77f9c18baf0
Santiago Arias <santi99oca@gmail.com>
2023-01-27 03:10:40 -0700

    Views for testing

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero{question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta numero {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Estas votando a la pregunta numero {question_id}')
