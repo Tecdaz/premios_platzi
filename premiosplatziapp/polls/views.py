23e21313e6f407075bc54f6cf29fc1d1cd64ae21
Santiago Arias <santi99oca@gmail.com>
2023-01-30 14:27:38 -0700

    Results view

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -23,7 +23,11 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f'Estas viendo los resultados de la pregunta numero {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
