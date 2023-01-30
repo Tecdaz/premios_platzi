98c9d63efc1e0c9d922595101c2e89e016a1818d
Santiago Arias <santi99oca@gmail.com>
2023-01-30 02:15:05 -0700

    Adding home page with a list of all questions

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("results/<int:question_id>/", views.results, name='results'),
     path("vote/<int:question_id>/", views.vote, name='vote'),
     path("detail/<int:question_id>/", views.detail, name='detail')
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = list(Question.objects.all())
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return render(request, 'polls/index.html', context=context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta numero{question_id}")
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
 
 
 def results(request, question_id):
