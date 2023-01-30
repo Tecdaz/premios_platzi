35d3b4059bcb06a261c17ac438db5e3c7d3238e2
Santiago Arias <santi99oca@gmail.com>
2023-01-30 16:03:21 -0700

    Generic views

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("results/<int:question_id>/", views.results, name='results'),
+    path("", views.IndexView.as_view(), name="index"),
+    path("results/<int:pk>/", views.ResultView.as_view(), name='results'),
     path("vote/<int:question_id>/", views.vote, name='vote'),
-    path("detail/<int:question_id>/", views.detail, name='detail')
+    path("detail/<int:pk>/", views.DetailView.as_view(), name='detail')
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultView(DetailView):
+    template_name = 'polls/results.html'
 
 
 def index(request):
