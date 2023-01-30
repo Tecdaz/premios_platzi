cbe8ff1d748b7bd2af2616156738e156279575f3
Santiago Arias <santi99oca@gmail.com>
2023-01-30 03:00:20 -0700

    Refactoring urls

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("results/<int:question_id>/", views.results, name='results'),
