8efa05ea2d7bef5d46e46ba2b4e91165efd05671
Santiago Arias <santi99oca@gmail.com>
2023-01-31 03:32:38 -0700

    Admin view

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register([Question, Choice])
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["question_text"]
+    inlines = [ChoiceInline]
+    list_display = ("question_text", "pub_date")
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
